a327214f7485428358b98b7036831cd7b2d04f80
Michael A Nowiszewski <micski@gmail.com>
2018-04-19 13:26:56 +0000

    Back in the fight - codefights

---
+++ Arcade/sortByHeight.js
@@ -0,0 +1,19 @@
+function sortByHeight(arr) {
+	var trees = [];
+	var sorted = [];
+	arr.map((a, b, c) => {
+		(a < 0) ? trees.push({
+			a,
+			b
+		}): sorted.push(a);
+	})
+	sorted.sort((a, b) => {
+		return a - b
+	})
+	trees.map((a, b, c) => {
+		sorted.splice(a.b, 0, a.a)
+	})
+	return sorted
+}
+var a = [-1, 150, 190, 170, -1, -1, 160, 180];
+sortByHeight(a)
